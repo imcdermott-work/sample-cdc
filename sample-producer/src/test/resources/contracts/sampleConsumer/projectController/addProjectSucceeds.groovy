93b96718c0f21b533ab1b5e052877b430c81b04e
imcdermott-work <imcdermott-work@users.noreply.github.com>
2018-02-26 12:42:20 +0000

    Update addProjectSucceeds.groovy

--- sample-producer/src/test/resources/contracts/sampleConsumer/projectController/addProjectSucceeds.groovy
+++ sample-producer/src/test/resources/contracts/sampleConsumer/projectController/addProjectSucceeds.groovy
@@ -4,9 +4,8 @@ org.springframework.cloud.contract.spec.Contract.make {
     method 'POST'
     url '/project/add'
     body([
-    //Here we are saying that the consumer should be able to pass in any hex value, but the test on the producer side should
-    //use B4311BB0A9C833BA3C0C3E723BF9C97B as
-   "name":$(consumer(regex('[a-zA-Z0-9]+')), producer('B4311BB0A9C833BA3C0C3E723BF9C97B')),
+    
+   "name":$(consumer(regex('[a-zA-Z0-9]+')), producer('ABC')),
    "users":[  
              [
                id: $(consumer(regex('[a-zA-Z0-9]+')), producer('111')),
@@ -21,9 +20,7 @@ org.springframework.cloud.contract.spec.Contract.make {
 response {
   status 200
   body(
-      //the body should contain the resourceId from the request in the case of the addSegment method.
       fromRequest().body('$.name')
-      //'B4311BB0A9C833BA3C0C3E723BF9C97B'
     )
  }
 }
